f469b6efe1ddc10517715f31ff8ac35017f41432
Duc Do <doduyminhduc@gmail.com>
2021-10-29 23:18:01 +0700

    add palindromer; roman converter

---
+++ javascripts/string_palindrome.js
@@ -0,0 +1,15 @@
+function palindrome(str) {
+
+    //lower cases all the letters remove all non-alphanumberic characters
+      str = str.lowercase().replace(/[\W_]+/g,"");
+    console.log(str);
+  
+    //how to turn a string to a palindrome
+  
+    //check palindrome
+  
+  
+    return true;
+  }
+  
+  console.log(palindrome("eye"));
